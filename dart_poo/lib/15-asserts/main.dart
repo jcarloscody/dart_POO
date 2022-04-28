48af2b18c57040e693525af041f572adf8dbda01
Josue <josuecarlosos2@gmail.com>
2022-04-28 10:28:18 -0300

    asserts

---
+++ dart_poo/lib/15-asserts/main.dart
@@ -0,0 +1,22 @@
+void main() {
+  //asserts: é uma afirmação, e com ele podemos fazer algumas verificações em tempo de dev. o objetivo é colocar uma validação nos atributos de uma class.
+  var c = Cliente();
+}
+
+class Cliente {
+  String? nome;
+  String? cpf;
+  String? cnpf;
+  String? razaoSocial;
+
+  Cliente({
+    this.cpf,
+    this.nome,
+    this.cnpf,
+    this.razaoSocial,
+  }) : assert(
+            (cpf != null)
+                ? nome != null && razaoSocial == null && cnpf == null
+                : true,
+            "Voce enviou cpf junto com cnpj");
+}
